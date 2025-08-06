2cc58779fa077e2a144417f43b3b507ddb4eb854
Oleksii Skrebtsov <alex.skrebtsov1213@gmail.com>
2025-08-06 12:07:27 +0000

    fix prettier

--- services/token-service.js
+++ services/token-service.js
@@ -2,56 +2,56 @@ const jwt = require("jsonwebtoken");
 const tokenModel = require("../models/token-model");
 
 class TokenService {
-  genereteTokens(payload) {
-    const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {
-      expiresIn: "30m",
-    });
-    const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {
-      expiresIn: "30d",
-    });
-    return {
-      accessToken,
-      refreshToken,
-    };
-  }
-
-  validateAccessToken(token) {
-    try {
-      const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
-      return userData;
-    } catch {
-      return null;
+    genereteTokens(payload) {
+        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {
+            expiresIn: "30m",
+        });
+        const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {
+            expiresIn: "30d",
+        });
+        return {
+            accessToken,
+            refreshToken,
+        };
     }
-  }
 
-  validateRefreshToken(token) {
-    try {
-      const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
-      return userData;
-    } catch {
-      return null;
+    validateAccessToken(token) {
+        try {
+            const userData = jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+            return userData;
+        } catch {
+            return null;
+        }
     }
-  }
 
-  async saveToken(userId, refreshToken) {
-    const tokenData = await tokenModel.findOne({ user: userId });
-    if (tokenData) {
-      tokenData.refreshToken = refreshToken;
-      return tokenData.save();
+    validateRefreshToken(token) {
+        try {
+            const userData = jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+            return userData;
+        } catch {
+            return null;
+        }
     }
-    const token = await tokenModel.create({ user: userId, refreshToken });
-    return token;
-  }
 
-  async removeToken(refreshToken) {
-    const tokenData = tokenModel.deleteOne({ refreshToken });
-    return tokenData;
-  }
+    async saveToken(userId, refreshToken) {
+        const tokenData = await tokenModel.findOne({ user: userId });
+        if (tokenData) {
+            tokenData.refreshToken = refreshToken;
+            return tokenData.save();
+        }
+        const token = await tokenModel.create({ user: userId, refreshToken });
+        return token;
+    }
 
-  async findToken(refreshToken) {
-    const tokenData = tokenModel.findOne({ refreshToken });
-    return tokenData;
-  }
+    async removeToken(refreshToken) {
+        const tokenData = tokenModel.deleteOne({ refreshToken });
+        return tokenData;
+    }
+
+    async findToken(refreshToken) {
+        const tokenData = tokenModel.findOne({ refreshToken });
+        return tokenData;
+    }
 }
 
 module.exports = new TokenService();
